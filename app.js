57cff2ff3ee444cc79d91d0eefcf26a83dd0f153
SAM224 <samcs224@hotmail.com>
2017-12-09 14:15:40 +0200

    add api controller.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -16,7 +17,9 @@ app.get('/', (req, res) => {
 });
 
 mongoose.connect(config.getDbConnextionString());
+
 setupController(app);
+apiController(app);
 
 app.listen(port, () => {
     console.log(`
